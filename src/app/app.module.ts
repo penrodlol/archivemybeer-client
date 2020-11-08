52f885b57008dbfea783b917ba2582bcf9a7bbea
penrodlol <penrod.christian7984@gmail.com>
2020-11-08 18:21:00 -0500

    🚀 feat: Startup Service * Init of startup service. * Injects icon registry for svgs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { SvgIconsModule } from '@ngneat/svg-icon';
 import { AppRoutingModule } from './app-routing.module';
 import { GraphQLModule } from './graphql.module';
 import { AppComponent } from './app.component';
+import { AppStartupService } from './app-startup.service';
 import icons from '../assets/svg/svg-icons';
 import { HeaderModule } from './header/header.module';
 
@@ -20,7 +21,15 @@ import { HeaderModule } from './header/header.module';
     GraphQLModule,
     SvgIconsModule.forRoot({ icons }),
   ],
-  providers: [],
+  providers: [
+    AppStartupService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (startup: AppStartupService) => () => startup.init(),
+      deps: [AppStartupService],
+      multi: true,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
